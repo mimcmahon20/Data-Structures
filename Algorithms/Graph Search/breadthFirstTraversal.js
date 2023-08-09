ca31c6b1fb715a52e30442903307c38bf06266f1
mimcmahon20 <mimcmahon20@vt.edu>
2023-08-09 14:35:57 -0400

    Create breadthFirstTraversal.js

---
+++ Algorithms/Graph Search/breadthFirstTraversal.js
@@ -0,0 +1,18 @@
+const breadthFirstTraversal = (start) => {
+    const visitedVertices = [start];
+    const visitQueue = new Queue();
+    visitQueue.enqueue(start);
+    while(!visitQueue.isEmpty()) {
+   const current = visitQueue.dequeue();
+    console.log(current.data);
+    current.edges.forEach((edge) => {
+      const neighbor = edge.end;
+  
+      if (!visitedVertices.includes(neighbor)) {
+        visitedVertices.push(neighbor);
+        visitQueue.enqueue(neighbor);
+      }
+    })
+    }
+   
+  };
